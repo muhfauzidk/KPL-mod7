c155e90e49972a3962723778a697846dffe881d5
Muhammad Fauzi Dwikurnia <muhfauzidk@gmail.com>
2022-04-09 14:03:17 +0700

    “aplikasi bank transfer dengan runtime configuration

--- modul7_1302194027/Program.cs
+++ modul7_1302194027/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+class BankTransferConfig
+{
+    public string lang { get; set; }
+    public transfer transfer { get; set; }
+    public string methods { get; set; }
+    public confirmation confirmation { get; set; }
+
+    public static void ReadJson()
+    {
+        using (StreamReader r = new StreamReader("bank_transfer_config.json"))
+        {
+            string json = r.ReadToEnd();
+            BankTransferConfig items = JsonConvert.DeserializeObject<BankTransferConfig>(json);
+
+            Console.WriteLine("{0}", items.lang);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BankTransferConfig.ReadJson();
+    }
+}
+
+public class transfer
+{
+    public string threshold { get; set;  }
+    public string low_fee { get; set; }
+    public string high_fee { get; set; }
+}
+
+public class confirmation
+{
+    public string yes { get; set; }
+    public string ya{ get; set; }
+}
